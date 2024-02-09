4c06876a279057f9e41258f9a175e4d69b8516d7
GarridoY <33898925+GarridoY@users.noreply.github.com>
2024-02-09 22:00:29 +0100

    Added simple CartController smoke test

---
+++ src/test/java/org/example/shoppingcartapi/smoketest/CartControllerSmokeTest.java
@@ -0,0 +1,20 @@
+package org.example.shoppingcartapi.smoketest;
+
+import org.example.shoppingcartapi.controller.CartController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class CartControllerSmokeTest {
+
+    @Autowired
+    private CartController controller;
+
+    @Test
+    void cartControllerLoads() {
+        assertThat(controller).isNotNull();
+    }
+}
