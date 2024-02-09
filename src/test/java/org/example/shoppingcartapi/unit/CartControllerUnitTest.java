034c9a0642c3e2d9f45131d492ea20b3411e2004
GarridoY <33898925+GarridoY@users.noreply.github.com>
2024-02-09 21:57:28 +0100

    Added CartController unit tests. Added ObjectMapper dependency
    Use ObjectMapper to create request bodies for testing endpoints

---
+++ src/test/java/org/example/shoppingcartapi/unit/CartControllerUnitTest.java
@@ -0,0 +1,54 @@
+package org.example.shoppingcartapi.unit;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.shoppingcartapi.contract.request.AddItemRequest;
+import org.example.shoppingcartapi.controller.CartController;
+import org.example.shoppingcartapi.service.CartService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CartController.class)
+public class CartControllerUnitTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private CartService cartService;
+
+    @Test
+    void allItemsInCartTest() throws Exception {
+        this.mockMvc.perform(get("/cart/{cartId}/items", 1L))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void addItemToCartTest() throws Exception {
+        this.mockMvc.perform(post("/cart/{cartId}/items", 1L)
+                        .content(objectMapper.writeValueAsString(new AddItemRequest(1L)))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteItemTest() throws Exception {
+        this.mockMvc.perform(delete("/cart/{cartId}/items/{itemId}", 1L, 1L))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void addItemToCartFailTest() throws Exception {
+        this.mockMvc.perform(post("/cart/{cartId}/items", 1L)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+}
